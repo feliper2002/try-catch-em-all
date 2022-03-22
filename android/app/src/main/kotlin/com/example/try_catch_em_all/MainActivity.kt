f758e5e9bc4fee01fb57d69c51fcad4473c3d590
Felipe Ribeiro <feliper.dev@gmail.com>
2022-03-21 22:33:40 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/try_catch_em_all/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.try_catch_em_all
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:try_catch_em_all/app/core/app_module.dart';
+import 'package:try_catch_em_all/app/core/app_widget.dart';
+
+void main() => runApp(ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ));
