6f6caf59dd364dc445f7757339afac36c446a991
Felipe Ribeiro <feliper.dev@gmail.com>
2022-03-23 22:16:04 -0300

    Pokedex

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:try_catch_em_all/app/core/app_module.dart';
 import 'package:try_catch_em_all/app/core/app_widget.dart';
+import 'package:uno/uno.dart';
 
 void main() => runApp(ModularApp(
       module: AppModule(),
